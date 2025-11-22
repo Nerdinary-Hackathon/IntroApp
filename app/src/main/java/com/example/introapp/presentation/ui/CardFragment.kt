9742c991f2b915ee41060c057fec451edb55fa9c
princehw03 <princehw03@gmail.com>
2025-11-23 00:11:35 +0900

    feat: 홈 화면  구현중

---
+++ app/src/main/java/com/example/introapp/presentation/ui/CardFragment.kt
@@ -0,0 +1,30 @@
+package com.example.introapp.presentation.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.introapp.R
+import com.example.introapp.databinding.FragmentCardBinding
+
+class CardFragment : Fragment() {
+    private lateinit var binding: FragmentCardBinding
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+//    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
