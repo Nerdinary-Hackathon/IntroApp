7aa89a77582b5aac4d0e97bc010f6442ea28b61c
ask0908 <askdfjaslk@naver.com>
2025-11-23 02:27:41 +0900

    refactor: 직무에 따라 온보딩 완료 화면에 다른 이미지 표시

--- app/src/main/java/com/example/introapp/presentation/ui/onboarding/OnboardingFragment5.kt
+++ app/src/main/java/com/example/introapp/presentation/ui/onboarding/OnboardingFragment5.kt
@@ -45,10 +45,11 @@ class OnboardingFragment5 : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setupObservers()
+        setupProfileImage()
 
         binding.run {
             val nickname = viewModel.onboardingData.value.page1.nickname
-            val descText = getString(R.string.profile_setting_complete_desc) // "님의 프로필이\n등록되었습니다."
+            val descText = getString(R.string.profile_setting_complete_desc)
             val fullText = "$nickname$descText"
             val spannable = SpannableString(fullText)
 
@@ -75,6 +76,25 @@ class OnboardingFragment5 : Fragment() {
                 viewModel.submitProfile()
             }
         }
+    }
+
+    /**
+     * 직무에 따라 프로필 이미지 다르게 설정
+     */
+    private fun setupProfileImage() {
+        val job = viewModel.onboardingData.value.page2.job
+
+        val imageRes = when (job) {
+            "PM" -> R.drawable.pm_onboarding_profile
+            "DESIGNER" -> R.drawable.design_onboarding_profile
+            "WEB" -> R.drawable.web_onboarding_profile
+            "BACKEND" -> R.drawable.backend_onboarding_profile
+            "ANDROID" -> R.drawable.android_onboarding_profile
+            "IOS" -> R.drawable.ios_onboarding_profile
+            else -> R.drawable.android_onboarding_profile // 기본값
+        }
+
+        binding.ivOnboardingProfile.setImageResource(imageRes)
     }
 
     private fun setupObservers() {
