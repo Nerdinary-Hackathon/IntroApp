9f5fccaf961f7c1a9f48852684010cdc9dbf4b25
princehw03 <princehw03@gmail.com>
2025-11-23 03:08:45 +0900

    feat: 홈액티비티 종속 2개 프래그먼트 구현 완료

--- app/src/main/java/com/example/introapp/presentation/ui/HomeActivity.kt
+++ app/src/main/java/com/example/introapp/presentation/ui/HomeActivity.kt
@@ -6,15 +6,48 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.introapp.R
+import com.example.introapp.databinding.ActivityHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.homeMain)) { v, insets ->
+
+        // 뷰바인딩 초기화
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //첫화면을 명함으로 설정
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.homeFrm, CardFragment())
+            .commit()
+
+        //상단 두 버튼에 클릭 리스너 적용
+        binding.tabs.addOnButtonCheckedListener { group, checkedId, isChecked ->
+            if (isChecked) {
+                when (checkedId) {
+                    R.id.tab_card -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.homeFrm, CardFragment())
+                            .commit()
+                    }
+                    R.id.tab_code -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.homeFrm, CodeFragment())
+                            .commit()
+                    }
+                }
+            }
+        }
+
+
+        // WindowInsets 적용
+        ViewCompat.setOnApplyWindowInsetsListener(binding.homeMain) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
